7056a812860c59b22a865930802352c6190d417b
kante.fode <fodekanteodc@gmail.com>
2023-07-20 11:16:40 +0000

    probleme1

---
+++ game2-quiz/src/main/java/com/quiz2/game2/quiz/Game2QuizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz2.game2.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game2QuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game2QuizApplication.class, args);
+	}
+
+}
---
+++ game2-quiz/src/test/java/com/quiz2/game2/quiz/Game2QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz2.game2.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game2QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
